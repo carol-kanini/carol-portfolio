7288e91ac8e53d4cd2afe39ed511ef37af1e5db0
edu <edmboche141@gmail.com>
2025-09-25 13:41:28 +0300

    added links to the nav icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo from '../assets/logo.png'
 import { FaLinkedin } from 'react-icons/fa'
-import { FaGithub } from 'react-icons/fa'
+import { FaGithub, FaWhatsapp } from 'react-icons/fa'
 import { FaSquareXTwitter } from 'react-icons/fa6'
 import { FaInstagram } from 'react-icons/fa6'
 import { FaFacebook } from 'react-icons/fa6'
@@ -15,10 +15,11 @@ const Navbar = () => {
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
             <FaLinkedin />
-            <FaGithub />
-            <FaSquareXTwitter />
-            <FaInstagram />
-            <FaFacebook />
+            <a href='https://github.com/eduu-11287'> <FaGithub /></a> 
+            <a href='https://wa.me/254717677853'> <FaWhatsapp /></a> 
+            <a href='https://x.com/Bumble_254'> <FaSquareXTwitter /></a>
+            <a href='https://www.instagram.com/eduuh_254/?__pwa=1'><FaInstagram /> </a>
+            <a href='https://www.facebook.com/Eduuh254'> <FaFacebook /></a>
         </div>
     </nav>
   )
